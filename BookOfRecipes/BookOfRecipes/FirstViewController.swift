6489e7305e0f5890ea4eb18f20845d362b5be32b
“allakin” <19paul91@mail.ru>
2019-02-03 21:54:49 +0300

    defaut file

---
+++ BookOfRecipes/BookOfRecipes/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BookOfRecipes
+//
+//  Created by Pavel on 03/02/2019.
+//  Copyright © 2019 Pavel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ BookOfRecipes/BookOfRecipes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BookOfRecipes
+//
+//  Created by Pavel on 03/02/2019.
+//  Copyright © 2019 Pavel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
